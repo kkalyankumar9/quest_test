5f72313a1af2d6f72742b0cfba61c819a4694dad
kkalyankumar9 <kummarikalyan010@gmail.com>
2024-03-16 16:55:32 +0530

    todo component completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { Box } from "@chakra-ui/react";
+import Todo from "./Todo";
 const Home = () => {
   return (
     <div>
-    
+      <Box display={"flex"} bgColor={"#7b8bdb"}>
+        <Todo />
+      </Box>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
